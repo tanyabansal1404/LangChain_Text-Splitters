36d542926c1ec92f021c7feb65580445305e2491
tanyabansal1404 <tanyabansal1404@gmail.com>
2026-01-31 16:45:34 +0530

    Initial Commit

---
+++ length_based.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+loader = PyPDFLoader('dl-curriculum.pdf')
+docs = loader.load()
+
+# text = """
+# We encourage pinning your version to a specific version in order to avoid breaking your CI when we publish new tests. We recommend upgrading to the latest version periodically to make sure you have the latest tests.
+
+# Not pinning your version will ensure you always have the latest tests, but it may also break your CI if we introduce tests that your integration doesn't pass.
+# """
+
+splitter = CharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=0,
+    separator=''
+)
+
+# result = splitter.split_text(docs[0].page_content)
+result = splitter.split_documents(docs)
+
+print(result[0].page_content)
